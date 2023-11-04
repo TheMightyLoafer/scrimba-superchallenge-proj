044d2bbc959b7abdde0c83f17f11bb4a691d7bd8
KingOfBoopp <147700520+TheMightyLoafer@users.noreply.github.com>
2023-11-04 02:17:03 -0700

    added scrimba version which uses destructoring

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ import propertyForSaleArr from "/properties/propertyForSaleArr"
 import placeholderPropertyObj from "./properties/placeholderPropertyObj"
 
 function getPropertyHtml(propertyArr = [placeholderPropertyObj]) {
-    return propertyArr.map(function (currentProperty) {
-        return`
+    return propertyArr.map(property => {
+        const {
+            propertyLocation,
+            priceGBP,
+            roomsM2,
+            comment,
+            image
+        } = property
+        const totalRoomSizeM2 = roomsM2.reduce((total, current) => total + current)
+        return `
     <section class="card">
-        <img src="/images/${propertyArr.image}">
+        <img src="/images/${image}">
             <div class="card-right">
-                <h2>${propertyArr.propertyLocation}</h2>
-                <h3>${propertyArr.priceGBP}</h3>
-                <p>${propertyArr.comment}</p>
-                <h3>${propertyArr.roomsM2} m&sup2;</h3>
+                <h2>${propertyLocation}</h2>
+                <h3>${priceGBP}</h3>
+                <p>${comment}</p>
+                <h3>${totalRoomSizeM2} m&sup2;</h3>
             </div>
-    </section> 
-
-        `
+    </section>`
     }).join('')
     /*
     SUPER CHALLENGE 💪
